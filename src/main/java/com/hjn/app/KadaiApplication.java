fa1b7f29bdf6518382c7845aea98012437302be3
muta <vvflowervv3jsbvv@gmail.com>
2023-04-12 23:14:02 +0900

    initial commit

---
+++ src/main/java/com/hjn/app/KadaiApplication.java
@@ -0,0 +1,13 @@
+package com.hjn.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KadaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KadaiApplication.class, args);
+	}
+
+}
